073e34bdc3e6900f74e725f81d82048588730ce7
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-27 17:05:20 +0200

    Parent Child relationship

---
+++ CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CoursesDto>> GetAuthorCourses(Guid authorId)
+        {
+            //Check if author exists
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var authorCourses = _courseLibraryRepository.GetCourses(authorId);
+
+            return Ok(_mapper.Map<IEnumerable<CoursesDto>>(authorCourses));
+        }
+    }
+}
