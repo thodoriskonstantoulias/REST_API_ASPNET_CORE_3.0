3624a556bba15c1026c5b3020a172c16b4571f36
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-27 13:29:27 +0200

    Status codes implementation

--- CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -22,14 +22,19 @@ namespace CourseLibrary.API.Controllers
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo); 
+            return Ok(authorsFromRepo); 
         }
 
         [HttpGet("{authorId}")]
         public IActionResult GetAuthor(Guid authorId)
         {
             var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
-            return new JsonResult(authorFromRepo);
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(authorFromRepo);
         }
     }
 }
