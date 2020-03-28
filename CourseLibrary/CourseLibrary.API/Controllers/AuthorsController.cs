78a06e660cf6fe21650428b997bc78884da96708
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-29 00:03:49 +0200

    DELETE request for parent elemet - cascade delete

--- CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -82,5 +82,21 @@ namespace CourseLibrary.API.Controllers
             Response.Headers.Add("Allow", "GET,OPTIONS,POST");
             return Ok();
         }
+
+        [HttpDelete("{authorId}")]
+        public ActionResult DeleteAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _courseLibraryRepository.DeleteAuthor(author);
+            _courseLibraryRepository.Save();
+
+            return NoContent();
+        }
     }
 }
